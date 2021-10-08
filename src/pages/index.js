67f52fed813b0d57d430cb251731ef2412187148
Cliff Wulfman <cwulfman@princeton.edu>
2021-10-07 20:08:47 -0400

    restored home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 const IndexPage = () => {
     return (
 	<Layout pageTitle="Black Death Digital Archive">
-		    <p>The BDDA is a multidisciplinary portal for researching the <a href="https://en.wikipedia.org/wiki/Second_plague_pandemic">Second Plague Pandemic</a>.</p>
+	    <p>BDDA is a multidisciplinary portal for researching the Second Plague Pandemic (outbreaks of plague that started with the Black Death and their recurrences across Afro-Eurasia from the 13th to the 19th centuries). The enormous body of evidence to reconstruct the pandemic has so far not been systematically compiled. This is the first scholarly initiative that aims to assemble all extant evidence (published and unpublished) on the pandemic and serve as a gateway for future research. This project collects different types of data—both documentary (archival, narrative) and material (bio-archeological, palaeogenomic) evidence—and assesses the evidence with the expertise of scholars working on different aspects of plague. As an open-access site, the BDDA portal will serve scholars, educators, students, and the wider public.</p>
 	</Layout>
 )}
 
